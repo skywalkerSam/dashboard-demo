a0d814f97c0fa0719ca8fc082fb939558360fc36
Starboy <samskywalker001@gmail.com>
2024-10-28 10:10:50 +0000

    minor tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import { ubuntu } from "@/app/ui/fonts";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: {
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   },
   description:
     "ASAI Inc. dashboard demo built w/ NEXT.js utilizing the new App Router.",
-  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+  metadataBase: new URL("https://dashboard-demo-umber.vercel.app/"),
 };
 export default function RootLayout({
   children,
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased bg-gray-600`}>{children}</body>
+      <body className={`${ubuntu.className} antialiased bg-gray-600`}>{children}</body>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+                port: '',
+                pathname: '/*',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
